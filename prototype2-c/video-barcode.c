4ff9fafa5350e8d82a4d2ec7b067889121434124
Sebastian Morr <sebastian@morr.cc>
2013-02-07 13:42:47 +0100

    Refactoring

--- prototype2-c/video-barcode.c
+++ prototype2-c/video-barcode.c
@@ -4,11 +4,7 @@
 
 #include <stdio.h>
 
-#define WIDTH 16
-#define HEIGHT 40
-#define LENGTH 1024
-
-void SaveFrame(AVFrame *pFrame, int width, int height) {
+void saveFrame(AVFrame *pFrame, int width, int height) {
     FILE *pFile;
     char *szFilename = "barcode.ppm";
     int  y;
@@ -30,7 +26,8 @@ void SaveFrame(AVFrame *pFrame, int width, int height) {
     fclose(pFile);
 }
 
-int main(int argc, char *argv[]) {
+int createBarcode(AVFrame *barcode, char *filename, int width, int height) {
+    int frameWidth = 16;
     AVFormatContext *pFormatCtx = NULL;
     int             i, videoStream;
     AVCodecContext  *pCodecCtx = NULL;
@@ -38,27 +35,21 @@ int main(int argc, char *argv[]) {
     AVFrame         *pFrame = NULL; 
     AVFrame         *pFrameRGB = NULL;
     AVFrame         *pFrameWide = NULL;
-    AVFrame         *pFrameBar = NULL;
     AVPacket        packet;
     int             frameFinished;
-    int             numBytes, numBytesWide, numBytesBar;
+    int             numBytes, numBytesWide;
     uint8_t         *buffer = NULL;
     uint8_t         *bufferWide = NULL;
-    uint8_t         *bufferBar = NULL;
 
     AVDictionary    *optionsDict = NULL;
     struct SwsContext      *sws_ctx = NULL;
     struct SwsContext      *sws_ctx2 = NULL;
 
-    if (argc < 2) {
-        printf("Please provide a movie file\n");
-        return -1;
-    }
     // Register all formats and codecs
     av_register_all();
 
     // Open video file
-    if (avformat_open_input(&pFormatCtx, argv[1], NULL, NULL) != 0)
+    if (avformat_open_input(&pFormatCtx, filename, NULL, NULL) != 0)
         return -1; // Couldn't open file
 
     // Retrieve stream information
@@ -66,7 +57,7 @@ int main(int argc, char *argv[]) {
         return -1; // Couldn't find stream information
 
     // Dump information about file onto standard error
-    av_dump_format(pFormatCtx, 0, argv[1], 0);
+    av_dump_format(pFormatCtx, 0, filename, 0);
 
     // Find the first video stream
     videoStream = -1;
@@ -97,39 +88,34 @@ int main(int argc, char *argv[]) {
     // Allocate an AVFrame structure
     pFrameRGB = avcodec_alloc_frame();
     pFrameWide = avcodec_alloc_frame();
-    pFrameBar = avcodec_alloc_frame();
     if (pFrameRGB == NULL)
         return -1;
 
     // Determine required buffer size and allocate buffer
-    numBytes = avpicture_get_size(PIX_FMT_RGB24, pCodecCtx->width, HEIGHT);
+    numBytes = avpicture_get_size(PIX_FMT_RGB24, pCodecCtx->width, height);
     buffer = (uint8_t *)av_malloc(numBytes*sizeof(uint8_t));
 
-    numBytesWide =avpicture_get_size(PIX_FMT_RGB24, WIDTH*LENGTH, HEIGHT);
+    numBytesWide =avpicture_get_size(PIX_FMT_RGB24, frameWidth*width, height);
     bufferWide = (uint8_t *)av_malloc(numBytesWide*sizeof(uint8_t));
 
-    numBytesBar = avpicture_get_size(PIX_FMT_RGB24, LENGTH, HEIGHT);
-    bufferBar = (uint8_t *)av_malloc(numBytesBar*sizeof(uint8_t));
-
     sws_ctx = sws_getContext(pCodecCtx->width, pCodecCtx->height, pCodecCtx->pix_fmt,
-                             WIDTH, HEIGHT, PIX_FMT_RGB24, SWS_BILINEAR, NULL, NULL, NULL);
+                             frameWidth, height, PIX_FMT_RGB24, SWS_BILINEAR, NULL, NULL, NULL);
 
-    sws_ctx2 = sws_getContext(WIDTH*LENGTH, HEIGHT, PIX_FMT_RGB24,
-                              LENGTH, HEIGHT, PIX_FMT_RGB24, SWS_BILINEAR, NULL, NULL, NULL);
+    sws_ctx2 = sws_getContext(frameWidth*width, height, PIX_FMT_RGB24,
+                              width, height, PIX_FMT_RGB24, SWS_BILINEAR, NULL, NULL, NULL);
 
     // Assign appropriate parts of buffer to image planes in pFrameRGB
     // Note that pFrameRGB is an AVFrame, but AVFrame is a superset
     // of AVPicture
-    avpicture_fill((AVPicture *)pFrameRGB, buffer, PIX_FMT_RGB24, WIDTH, HEIGHT);
-    avpicture_fill((AVPicture *)pFrameWide, bufferWide, PIX_FMT_RGB24, WIDTH*LENGTH, HEIGHT);
-    avpicture_fill((AVPicture *)pFrameBar, bufferBar, PIX_FMT_RGB24, LENGTH, HEIGHT);
+    avpicture_fill((AVPicture *)pFrameRGB, buffer, PIX_FMT_RGB24, frameWidth, height);
+    avpicture_fill((AVPicture *)pFrameWide, bufferWide, PIX_FMT_RGB24, frameWidth*width, height);
 
-    int64_t seconds_per_frame = pFormatCtx->duration/LENGTH;
+    int64_t seconds_per_frame = pFormatCtx->duration/width;
 
     uint8_t *orig = pFrameWide->data[0];
 
     int try;
-    for (i = 0; i<LENGTH; i++) {
+    for (i = 0; i<width; i++) {
         printf("%d\n", i);
         av_seek_frame(pFormatCtx, -1, i*seconds_per_frame, 0);
         frameFinished = 0;
@@ -155,29 +141,50 @@ int main(int argc, char *argv[]) {
             // Convert the image from its native format to RGB
             sws_scale(sws_ctx, (uint8_t const * const *)pFrame->data, pFrame->linesize, 0, pCodecCtx->height,
                       pFrameWide->data, pFrameWide->linesize);
-            pFrameWide->data[0] += WIDTH*3;
+            pFrameWide->data[0] += frameWidth*3;
         }
     }
 
     pFrameWide->data[0] = orig;
-    sws_scale(sws_ctx2, (uint8_t const * const *)pFrameWide->data, pFrameWide->linesize, 0, HEIGHT,
-              pFrameBar->data, pFrameBar->linesize);
-
-    SaveFrame(pFrameBar, LENGTH, HEIGHT);
+    sws_scale(sws_ctx2, (uint8_t const * const *)pFrameWide->data, pFrameWide->linesize, 0, height,
+              barcode->data, barcode->linesize);
 
     av_free(buffer);
     av_free(bufferWide);
-    av_free(bufferBar);
     av_free(pFrame);
     av_free(pFrameRGB);
     av_free(pFrameWide);
-    av_free(pFrameBar);
 
     // Close the codec
     avcodec_close(pCodecCtx);
 
     // Close the video file
     avformat_close_input(&pFormatCtx);
+
+    return 0;
+}
+
+int main(int argc, char *argv[]) {
+    if (argc < 2) {
+        printf("Please provide a movie file\n");
+        return -1;
+    }
+
+    int width = 1024;
+    int height = 40;
+
+    AVFrame *barcode;
+    uint8_t *barcodeBuffer = NULL;
+
+    barcode = avcodec_alloc_frame();
+    barcodeBuffer = (uint8_t *)av_malloc(sizeof(uint8_t)*avpicture_get_size(PIX_FMT_RGB24, width, height));
+    avpicture_fill((AVPicture *)barcode, barcodeBuffer, PIX_FMT_RGB24, width, height);
+
+    createBarcode(barcode, argv[1], width, height);
+    saveFrame(barcode, width, height);
+
+    av_free(barcodeBuffer);
+    av_free(barcode);
 
     return 0;
 }
