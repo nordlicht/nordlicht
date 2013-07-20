7e1fca345f73a2b9dd16502ae7fb4c0babc0168d
Sebastian Morr <sebastian@morr.cc>
2013-07-20 15:52:57 +0200

    tool: output_path is $(basename input_path).png

--- src/nordlicht-tool.c
+++ src/nordlicht-tool.c
@@ -7,42 +7,35 @@ char *gnu_basename(char *path) {
     return base ? base+1 : path;
 }
 
-char *remove_ext(char *filename) {
-    char *retstr;
-    if ((retstr = malloc (strlen (filename) + 1)) == NULL)
-        return NULL;
-    strcpy (retstr, filename);
-    char *lastdot = strrchr (retstr, '.');
-    if (lastdot != NULL)
-        *lastdot = '\0';
-    return retstr;
-}
-
 int main(int argc, char** argv) {
     int width = 1024;
     int height = 400;
     char *file_path = NULL;
 
     if (argc < 2) {
-        printf("Please provide a video file\n");
+        printf("Usage: nordlicht VIDEOFILE [WIDTH [HEIGHT [BARCODEFILE.png]]]\nDefault size is %dx%d, default BARCODEFILE is \"$(basename VIDEOFILE).png\".\n", width, height);
         return 1;
     }
+
+    file_path = argv[1];
+    char *output_path = malloc(snprintf(NULL, 0, "%s.png", gnu_basename(file_path)) + 1);
+    sprintf(output_path, "%s.png", gnu_basename(file_path));
+
     if (argc >= 3) {
         width = atoi(argv[2]);
     }
     if (argc >= 4) {
         height = atoi(argv[3]);
     }
-    file_path = argv[1];
+    if (argc >= 5) {
+        free(output_path);
+        output_path = argv[4];
+    }
 
     nordlicht *code;
     nordlicht_create(&code, width, height);
 
-    char *basename = gnu_basename(file_path);
-    char *output_path = remove_ext(basename);
-    strcat(output_path, ".png");
     nordlicht_output(code, output_path);
-
     nordlicht_input(code, file_path);
 
     while (!nordlicht_is_done(code)) {
