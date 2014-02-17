c44ee14801bb45044a6fff4c68aab07c3110e549
Nikoli <nikoli@gmx.us>
2014-02-17 12:32:08 +0400

    Improve tests, update gitignore
    Script does not work with dash, print some progress,
    run tests in subdir: allows several users to run tests simultaneously,
    remove temporary files, add more tests, do not use system copy of cli.

--- test/test-tool.sh
+++ test/test-tool.sh
@@ -1,11 +1,12 @@
-#!/bin/sh
+#!/bin/bash
 
 pass() {
-    $@ &> /tmp/nordlicht_test
+    echo "Running '$@'"
+    $@ &> "$LOG"
     exit_code=$?
 
     if test $exit_code != 0; then
-        cat /tmp/nordlicht_test
+        cat "$LOG"
         echo "command failed: $*"
         exit 1
     fi
@@ -13,11 +14,12 @@ pass() {
 }
 
 fail() {
-    $@ &> /tmp/nordlicht_test
+    echo "Running '$@'"
+    $@ &> "$LOG"
     exit_code=$?
 
     if test $exit_code = 0; then
-        cat /tmp/nordlicht_test
+        cat "$LOG"
         echo "command succeded: $*"
         exit 1
     elif test $exit_code -gt 129 -a $exit_code -le 192; then
@@ -33,9 +35,14 @@ fail() {
     return 0
 }
 
-PATH=.:/bin:/usr/bin:/usr/sbin
-VIDEO="video.mp4"
-OUTPUT="output.png"
+VIDEO='video.mp4'
+LOG='test.log'
+TMP="tests-tmp_$$/"
+
+mkdir "$TMP" &&
+ln -s ../nordlicht "$TMP" &&
+ln -s ../"$VIDEO" "$TMP" &&
+pushd "$TMP" || exit 1
 
 # test environment
 pass true
@@ -43,36 +50,45 @@ fail false
 pass test -f "$VIDEO"
 
 # argument parsing
-pass nordlicht --help
-fail nordlicht
-fail nordlicht --fubar
-fail nordlicht "$VIDEO" somethingelse
+pass ./nordlicht --help
+fail ./nordlicht
+fail ./nordlicht --fubar
+fail ./nordlicht "$VIDEO" somethingelse
 
 # input file
-fail nordlicht nonexistantvideo.foo
+fail ./nordlicht nonexistantvideo.foo
 
 # size
-pass nordlicht "$VIDEO" -w 1 -h 1
-fail nordlicht "$VIDEO" -w huuuge
-fail nordlicht "$VIDEO" -w 0
-fail nordlicht "$VIDEO" -h 0
-fail nordlicht "$VIDEO" -w -100
-fail nordlicht "$VIDEO" -h -100
+pass ./nordlicht "$VIDEO" -w 1 -h 1
+fail ./nordlicht "$VIDEO" -w huuuge
+fail ./nordlicht "$VIDEO" -w 0
+fail ./nordlicht "$VIDEO" -h 0
+fail ./nordlicht "$VIDEO" -w ''
+fail ./nordlicht "$VIDEO" -h ''
+fail ./nordlicht "$VIDEO" -w -100
+fail ./nordlicht "$VIDEO" -h -100
+fail ./nordlicht "$VIDEO" -w 1.1
+fail ./nordlicht "$VIDEO" -h 1.1
+fail ./nordlicht "$VIDEO" -w 1,1
+fail ./nordlicht "$VIDEO" -h 1,1
 
 # output file
-pass nordlicht "$VIDEO" -w 1 -o unicode.png
-pass nordlicht "$VIDEO" -w 1 -o ünîç⌀də.png
+pass ./nordlicht "$VIDEO" -w 1 -o unicode.png
+pass ./nordlicht "$VIDEO" -w 1 -o ünîç⌀də.png
 pass test -f ünîç⌀də.png
-fail nordlicht "$VIDEO" -o /tmp
-fail nordlicht "$VIDEO" -o "$VIDEO"
-fail nordlicht "$VIDEO" -o ""
+fail ./nordlicht "$VIDEO" -o ../"$TMP"
+fail ./nordlicht "$VIDEO" -o "$VIDEO"
+fail ./nordlicht "$VIDEO" -o ''
 
 # style
-pass nordlicht "$VIDEO" -w 1 -s vertical
-pass nordlicht "$VIDEO" -w 1 -s horizontal
-fail nordlicht "$VIDEO" -s nope
+pass ./nordlicht "$VIDEO" -w 1 -s vertical
+pass ./nordlicht "$VIDEO" -w 1 -s horizontal
+fail ./nordlicht "$VIDEO" -s nope
+fail ./nordlicht "$VIDEO" -s ''
 
 # exact seeking
-pass nordlicht "$VIDEO" -w 1 -e
+pass ./nordlicht "$VIDEO" -w 1 -e
+
+popd && rm -rv "$TMP" || exit 1
 
 echo "All assertions passed. Yay!"
