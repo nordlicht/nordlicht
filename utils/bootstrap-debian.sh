5aae6e15c8fb5982a39a5a7db79d97587a0844e5
Sebastian Morr <sebastian@morr.cc>
2014-03-01 20:37:10 +0100

    Vagrant: Ubuntu 12.04 does not install debhelper as a dependency

--- utils/bootstrap-debian.sh
+++ utils/bootstrap-debian.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 apt-get update &&
-apt-get install -y cmake libavcodec-dev libswscale-dev libavformat-dev libfreeimage-dev libpopt-dev git-buildpackage help2man
+apt-get install -y cmake libavcodec-dev libswscale-dev libavformat-dev libfreeimage-dev libpopt-dev git-buildpackage debhelper help2man
