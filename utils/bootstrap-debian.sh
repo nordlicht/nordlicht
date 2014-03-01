c0e588b7210935d3fca026f2b6e064005fa85d15
Sebastian Morr <sebastian@morr.cc>
2014-03-01 20:32:53 +0100

    Vagrant: Don't apt-get upgrade the VMs

--- utils/bootstrap-debian.sh
+++ utils/bootstrap-debian.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
 
 apt-get update &&
-apt-get upgrade &&
 apt-get install -y cmake libavcodec-dev libswscale-dev libavformat-dev libfreeimage-dev libpopt-dev git-buildpackage help2man
