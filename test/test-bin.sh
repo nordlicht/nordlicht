166e05baf3c85a3dfd1515299d657a7fde414529
Sebastian Morr <sebastian@morr.cc>
2014-03-03 22:33:35 +0100

    Add test: "-h 10000" should pass

--- test/test-bin.sh
+++ test/test-bin.sh
@@ -60,6 +60,7 @@ fail ./nordlicht nonexistantvideo.foo
 
 # size
 pass ./nordlicht "$VIDEO" -w 1 -h 1
+pass ./nordlicht "$VIDEO" -w 1 -h 10000
 fail ./nordlicht "$VIDEO" -w huuuge
 fail ./nordlicht "$VIDEO" -w 0
 fail ./nordlicht "$VIDEO" -h 0
@@ -73,7 +74,6 @@ fail ./nordlicht "$VIDEO" -w 1,1
 fail ./nordlicht "$VIDEO" -h 1,1
 
 # output file
-pass ./nordlicht "$VIDEO" -w 1 -o unicode.png
 pass ./nordlicht "$VIDEO" -w 1 -o ünîç⌀də.png
 pass test -f ünîç⌀də.png
 fail ./nordlicht "$VIDEO" -o ../"$TMP"
@@ -85,9 +85,6 @@ pass ./nordlicht "$VIDEO" -w 1 -s vertical
 pass ./nordlicht "$VIDEO" -w 1 -s horizontal
 fail ./nordlicht "$VIDEO" -s nope
 fail ./nordlicht "$VIDEO" -s ''
-
-# exact seeking
-pass ./nordlicht "$VIDEO" -w 1 -e
 
 popd && rm -rv "$TMP" || exit 1
 
