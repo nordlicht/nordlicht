228144fbd92ca25b41746efb5bad51dcd14452e6
Sebastian Morr <sebastian@morr.cc>
2014-03-16 19:40:22 +0100

    Add simple library testsuite and update tool testsuite
    Both should be run with "make test" from the build directory and require a
    "video.mp4" to be present.

---
+++ test/library.c
@@ -0,0 +1,102 @@
+#include <assert.h>
+#include <limits.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <stdarg.h>
+#include <unistd.h>
+#include "nordlicht.h"
+
+#define fail(call) assert(0 != (call))
+#define pass(call) assert(0 == (call))
+#define null(call) assert(NULL == (call))
+
+int file_exists(char *filename) {
+    return access(filename, F_OK) != -1;
+}
+
+int main(void) {
+    nordlicht *n;
+    char *VID = "video.mp4";
+
+    // test environment
+    assert(1);
+    assert(!0);
+    fail(-1);
+    pass(0);
+    assert(file_exists(VID));
+
+    // invalid input
+    null(nordlicht_init(NULL, 100, 100, 0));
+    null(nordlicht_init("", 100, 100, 0));
+    null(nordlicht_init("\0", 100, 100, 0));
+    null(nordlicht_init(".", 100, 100, 0));
+    null(nordlicht_init("..", 100, 100, 0));
+    null(nordlicht_init("nonexistant_file.123", 100, 100, 0));
+
+    // invalid size
+    null(nordlicht_init(VID, 0, 100, 0));
+    null(nordlicht_init(VID, 100, 0, 0));
+    null(nordlicht_init(VID, 0, 0, 0));
+    null(nordlicht_init(VID, -100, 100, 0));
+    null(nordlicht_init(VID, 100, -100, 0));
+    null(nordlicht_init(VID, INT_MIN, INT_MIN, 0));
+
+    // valid size
+    assert(nordlicht_init(VID, 1, 1, 0));
+    assert(nordlicht_init(VID, INT_MAX, INT_MAX, 0));
+
+    // invalid output
+    n = nordlicht_init(VID, 1, 100, 0);
+    assert(n);
+    fail(nordlicht_write(n, NULL));
+    fail(nordlicht_write(n, ""));
+    fail(nordlicht_write(n, "\0"));
+    fail(nordlicht_write(n, "."));
+    fail(nordlicht_write(n, ".."));
+    fail(nordlicht_write(n, VID));
+    nordlicht_free(n);
+
+    // style
+    n = nordlicht_init(VID, 1, 100, 0);
+    assert(n);
+    pass(nordlicht_set_style(n, NORDLICHT_STYLE_HORIZONTAL));
+    pass(nordlicht_set_style(n, NORDLICHT_STYLE_VERTICAL));
+    fail(nordlicht_set_style(n, 2));
+    fail(nordlicht_set_style(n, 1000000));
+    fail(nordlicht_set_style(n, -1));
+    nordlicht_free(n);
+
+    // buffer
+    const unsigned char *buffer = NULL;
+    n = nordlicht_init(VID, 2, 100, 0);
+    assert(n);
+    buffer = nordlicht_buffer(n);
+    assert(buffer);
+    assert(2*100*4 == nordlicht_buffer_size(n));
+    unsigned char *buffer2 = NULL;
+    fail(nordlicht_set_buffer(n, buffer2));
+    buffer2 = malloc(nordlicht_buffer_size(n));
+    pass(nordlicht_set_buffer(n, buffer2));
+    pass(nordlicht_set_buffer(n, buffer2));
+    pass(nordlicht_set_buffer(n, buffer2));
+    buffer = nordlicht_buffer(n);
+    assert(buffer == buffer2);
+    nordlicht_free(n);
+    free(buffer2);
+
+    // complete run
+    n = nordlicht_init(VID, 1, 100, 0);
+    assert(n);
+    assert(0 == nordlicht_progress(n));
+    pass(nordlicht_generate(n));
+    assert(1 == nordlicht_progress(n));
+    fail(nordlicht_set_style(n, NORDLICHT_STYLE_HORIZONTAL));
+    buffer2 = malloc(nordlicht_buffer_size(n));
+    fail(nordlicht_set_buffer(n, buffer2));
+    nordlicht_free(n);
+    free(buffer2);
+
+    printf("--- nordlicht library test suite passed. ---\n");
+    return 0;
+}
---
+++ test/tool.sh
@@ -0,0 +1,101 @@
+#!/bin/bash
+
+nordlicht() {
+    ../nordlicht $@
+}
+
+abort() {
+    popd
+    rm -r "$TMP"
+    exit 1
+}
+
+pass() {
+    echo "Running '$@'"
+    $@ &> "$LOG"
+    exit_code=$?
+
+    if test $exit_code != 0; then
+        cat "$LOG"
+        echo "command failed: $*"
+        abort
+    fi
+    return 0
+}
+
+fail() {
+    echo "Running '$@'"
+    $@ &> "$LOG"
+    exit_code=$?
+
+    if test $exit_code = 0; then
+        cat "$LOG"
+        echo "command succeded: $*"
+        abort
+    elif test $exit_code -gt 129 -a $exit_code -le 192; then
+        echo "died by signal: $*"
+        abort
+    elif test $exit_code = 127; then
+        echo "command not found: $*"
+        abort
+    elif test $exit_code = 126; then
+        echo "valgrind error: $*"
+        abort
+    fi
+    return 0
+}
+
+LOG='test.log'
+TMP="test_tmp_$$/"
+
+mkdir "$TMP" &&
+pushd "$TMP" || exit 1
+
+VIDEO='../video.mp4'
+
+# test environment
+pass true
+fail false
+pass test -f "$VIDEO"
+
+# argument parsing
+pass nordlicht --help
+fail nordlicht
+fail nordlicht --fubar
+fail nordlicht "$VIDEO" somethingelse
+
+# input file
+fail nordlicht nonexistantvideo.foo
+
+# size
+pass nordlicht "$VIDEO" -w 1 -h 1
+pass nordlicht "$VIDEO" -w 1 -h 10000
+fail nordlicht "$VIDEO" -w huuuge
+fail nordlicht "$VIDEO" -w 0
+fail nordlicht "$VIDEO" -h 0
+fail nordlicht "$VIDEO" -w ''
+fail nordlicht "$VIDEO" -h ''
+fail nordlicht "$VIDEO" -w -100
+fail nordlicht "$VIDEO" -h -100
+fail nordlicht "$VIDEO" -w 1.1
+fail nordlicht "$VIDEO" -h 1.1
+fail nordlicht "$VIDEO" -w 1,1
+fail nordlicht "$VIDEO" -h 1,1
+
+# output file
+pass nordlicht "$VIDEO" -w 1 -o ünîç⌀də.png
+pass test -f ünîç⌀də.png
+fail nordlicht "$VIDEO" -o ../"$TMP"
+fail nordlicht "$VIDEO" -o "$VIDEO"
+fail nordlicht "$VIDEO" -o ''
+
+# style
+pass nordlicht "$VIDEO" -w 1 -s vertical
+pass nordlicht "$VIDEO" -w 1 -s horizontal
+fail nordlicht "$VIDEO" -s nope
+fail nordlicht "$VIDEO" -s ''
+
+popd
+rm -r "$TMP"
+
+echo "--- nordlicht tool test suite passed. ---"
