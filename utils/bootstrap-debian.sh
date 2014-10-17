89e3551c43e63963b71245f4941ccbe6ced6f244
Sebastian Morr <sebastian@morr.cc>
2014-10-17 17:17:10 +0200

    Remove Vagrantfile and related files
    It seems to clutter this project and belongs somewhere else.

--- utils/bootstrap-debian.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-apt-get update &&
-apt-get install -y cmake libavcodec-dev libswscale-dev libavformat-dev libpng-dev libpopt-dev git-buildpackage debhelper help2man python-software-properties
