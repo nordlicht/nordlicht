6e2e9527daaf02a13c3053603e826a44612e5150
Sebastian Morr <sebastian@morr.cc>
2016-01-04 18:51:40 +0100

    [mpv] Wait for fullscreen, seek on click

--- utils/mpv-nordlicht.lua
+++ utils/mpv-nordlicht.lua
@@ -3,15 +3,10 @@
 
 function init()
     is_on = false
-    mode = 1 -- 1: single, 2: double
-    -- size of the display:
-    screen_width = mp.get_property("osd-width")
 
     -- size of the barcode:
     width = mp.get_property("osd-width")
     height = math.floor(width/20)
-    width2 = width*5
-    height2 = math.floor(width2/50)
 
     -- size of the progress marker:
     mh = math.floor(height/15)*2+1
@@ -20,7 +15,7 @@ function init()
     -- styles:
     styles = os.getenv("NORDLICHT_STYLE") or "horizontal"
 
-    mp.register_event("start-file", new_file)
+    mp.register_event("file-loaded", new_file)
     mp.register_event("shutdown", shutdown)
     mp.add_key_binding("n", "nordlicht-toggle", toggle)
 
@@ -51,12 +46,9 @@ function new_file()
     kill()
 
     local nordlicht_cmd = "nordlicht -s "..styles
-    local nordlicht_cmd2 = "nordlicht -s "..styles
     local path = mp.get_property("path")
     local cmd = "nice "..nordlicht_cmd.." \""..path.."\" -o /tmp/nordlicht.bgra -w "..width.." -h "..height.." &"
     os.execute(cmd)
-    local cmd2 = "nice "..nordlicht_cmd2.." \""..path.."\" -o /tmp/nordlicht2.bgra -w "..width2.." -h "..height2.." &"
-    os.execute(cmd2)
 
     if was_on then
         -- wait for the file to be opened and truncated
@@ -71,16 +63,6 @@ function update()
         mp.command("overlay_add 0 0 "..mh.." /tmp/nordlicht.bgra 0 bgra "..width.." "..height.." "..width*4)
         mp.command("overlay_add 1 "..(math.floor(pos/100*width)-(mw-1)/2).." "..(0).." /tmp/arrow_down.bgra 0 bgra "..mw.." "..mh.." "..mw*4)
         mp.command("overlay_add 2 "..(math.floor(pos/100*width)-(mw-1)/2).." "..(height+mh).." /tmp/arrow_up.bgra 0 bgra "..mw.." "..mh.." "..mw*4)
-    end
-
-    if mode == 2 then
-        local offset = height+3*mh
-
-        if pos ~= nil then
-            mp.command("overlay_add 3 "..math.floor(screen_width/2-width2*pos/100).." "..(mh+offset).." /tmp/nordlicht2.bgra 0 bgra "..width2.." "..height2.." "..width2*4)
-            mp.command("overlay_add 4 "..(math.floor(screen_width/2)-(mw-1)/2).." "..(offset).." /tmp/arrow_down.bgra 0 bgra "..mw.." "..mh.." "..mw*4)
-            mp.command("overlay_add 5 "..(math.floor(screen_width/2)-(mw-1)/2).." "..(height2+mh+offset).." /tmp/arrow_up.bgra 0 bgra "..mw.." "..mh.." "..mw*4)
-        end
     end
 end
 
@@ -97,9 +79,6 @@ function off()
         mp.command("overlay_remove 0")
         mp.command("overlay_remove 1")
         mp.command("overlay_remove 2")
-        mp.command("overlay_remove 3")
-        mp.command("overlay_remove 4")
-        mp.command("overlay_remove 5")
         is_on = false
     end
 end
@@ -107,20 +86,28 @@ end
 function toggle()
     if is_on then
         off()
-        if mode == 1 then
-            mode = 2
-            on()
-        end
     else
-        mode = 1
         on()
     end
 end
 
-function fullscreen()
-    mp.set_property("fullscreen", "yes")
+mp.add_key_binding("mouse_btn0", "jump",
+    function(e)
+        local mouseX, mouseY = mp.get_mouse_pos()
+        local osdX, osdY = mp.get_osd_resolution()
+        mouseX = 100.0*mouseX/osdX
+
+        mp.commandv("seek", mouseX, "absolute-percent", "exact")
+    end
+)
+
+function maybeinit()
+    if tonumber(mp.get_property("osd-width")) > 0 then
+        init()
+    else
+        mp.add_timeout(0.1, maybeinit)
+    end
 end
 
--- not an optimal solution, but it seems to work:
-mp.add_timeout(0.5, fullscreen)
-mp.add_timeout(2, init)
+mp.set_property("fullscreen", "yes")
+maybeinit()
