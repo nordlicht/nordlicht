c25be3452883f7d0c5b73b51536846c7de9ae7cc
Sebastian Morr <sebastian@morr.cc>
2017-03-14 09:02:30 +0100

    URL of testfile on Mediawiki has changed

--- cmake/download_testfile.cmake
+++ cmake/download_testfile.cmake
@@ -1,4 +1,4 @@
 if(NOT EXISTS ${CMAKE_BINARY_DIR}/video.mp4)
     message("Downloading 3 MB test file, just a sec...")
-    file(DOWNLOAD https://upload.wikimedia.org/wikipedia/commons/0/07/Sintel_excerpt.OGG ${CMAKE_BINARY_DIR}/video.mp4 EXPECTED_MD5 be22cb1f83f6f3620d8048df12f9f52d)
+    file(DOWNLOAD https://upload.wikimedia.org/wikipedia/commons/6/64/Sintel_excerpt.ogv ${CMAKE_BINARY_DIR}/video.mp4 EXPECTED_MD5 be22cb1f83f6f3620d8048df12f9f52d)
 endif()
